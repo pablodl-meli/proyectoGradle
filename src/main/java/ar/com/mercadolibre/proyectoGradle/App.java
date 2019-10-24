db9bb54c1cee2050c05f87fff2a27e2c6119284c
Pablo Diaz Legaspi <pdiazlegaspi@ar0fvfyq18chv2j.ml.com>
2019-10-24 09:55:44 -0300

    Modifica el saludo, retorna null ahora

--- src/main/java/ar/com/mercadolibre/proyectoGradle/App.java
+++ src/main/java/ar/com/mercadolibre/proyectoGradle/App.java
@@ -5,7 +5,7 @@ package ar.com.mercadolibre.proyectoGradle;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return null;
     }
 
     public static void main(String[] args) {
